351a30c0c84c926d84a8f7e619d744e159b25c2e
Sabitibas <ngafgwejh@gmail.com>
2026-06-16 14:25:12 +0700

    ubah threshold notif ke M5

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def cek_dan_kirim_notif(gempa):
 
     if gempa:
         id_sekarang = gempa.get("DateTime", "")
-        if float(gempa["Magnitude"]) >= 3.5:
+        if float(gempa["Magnitude"]) >= 5.0:
             if id_sekarang != gempa_terakhir_notif:
                 kirim_notif_telegram(gempa)
                 gempa_terakhir_notif = id_sekarang
